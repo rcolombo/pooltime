285b2f96e5bf9c8bced69a7a29826d2a19013b2a
Joe Zirilli <joezirilli@gmail.com>
2014-12-08 18:51:54 -0500

    Moved week picker out of header and to picks and all-picks screens

--- pooltime/client/app/scripts/directives/directives.js
+++ pooltime/client/app/scripts/directives/directives.js
@@ -1,6 +1,6 @@
 (function (angular) {
     'use strict';
 
-    angular.module('directives', ['directives.header', 'directives.topindicator']);
+    angular.module('directives', ['directives.header', 'directives.topindicator', 'directives.weekpicker']);
 
 })(angular);
