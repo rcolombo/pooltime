1f4203157e009c51a58204ed3cc22755a5db741c
Joe Zirilli <joezirilli@gmail.com>
2014-12-08 19:15:57 -0500

    Refactored fabfile to use actual names of tasks in run task options. Also made it explicitly call the Grunt build task, instead of the grunt default task.

--- fabfile.py
+++ fabfile.py
@@ -4,29 +4,42 @@ from fabric.api import task
 
 from fabric.operations import local
 
+class TaskHelper:
+    @staticmethod
+    def serve():
+        local('cd %s && python pooltime/app.py' % os.path.dirname(__file__))
+
+    @staticmethod
+    def install():
+        local('cd %s && npm install && bower install' % os.path.join(os.path.dirname(__file__), 'pooltime/client'))
+
+    @staticmethod
+    def build():
+        local('cd %s && grunt build' % os.path.join(os.path.dirname(__file__), 'pooltime/client'))
+        local('rm -rf %s' % os.path.join(os.path.dirname(__file__), 'pooltime/static'))
+        local('mv %s %s' % (os.path.join(os.path.dirname(__file__), 'pooltime/client/dist/static'), os.path.join(os.path.dirname(__file__), 'pooltime')))
+        local('mv %s %s' % (os.path.join(os.path.dirname(__file__), 'pooltime/client/dist/index.html'), os.path.join(os.path.dirname(__file__), 'pooltime/templates')))
+
 @task
-def run(server=True, prep=False, make=False):
-    if true(prep):
-        install()
-    if true(make):
-        build()
-    if true(server):
-        serve()
+def run(serve=True, install=False, build=False):
+    if true(install):
+        TaskHelper.install()
+    if true(build):
+        TaskHelper.build()
+    if true(serve):
+        TaskHelper.serve()
 
 @task
 def serve():
-    local('cd %s && python pooltime/app.py' % os.path.dirname(__file__))
+    TaskHelper.serve()
 
 @task
 def install():
-    local('cd %s && npm install && bower install' % os.path.join(os.path.dirname(__file__), 'pooltime/client'))
+    TaskHelper.install()
 
 @task
 def build():
-    local('cd %s && grunt' % os.path.join(os.path.dirname(__file__), 'pooltime/client'))
-    local('rm -rf %s' % os.path.join(os.path.dirname(__file__), 'pooltime/static'))
-    local('mv %s %s' % (os.path.join(os.path.dirname(__file__), 'pooltime/client/dist/static'), os.path.join(os.path.dirname(__file__), 'pooltime')))
-    local('mv %s %s' % (os.path.join(os.path.dirname(__file__), 'pooltime/client/dist/index.html'), os.path.join(os.path.dirname(__file__), 'pooltime/templates')))
+    TaskHelper.build()
 
 def true(o):
     return o in ['True', 'true', True]
